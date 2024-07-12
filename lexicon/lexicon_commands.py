36db99f3a73ef0e30a51c7e20a327dc94a386585
Vuacheslav <soloomonslava1991@gmail.com>
2024-07-12 16:09:16 +0400

    add lexicon_commands dict

---
+++ lexicon/lexicon_commands.py
@@ -0,0 +1,7 @@
+LEXICON_COMMANDS: dict[str, str] = {
+    '/start': 'Старт бота',
+    '/equip': 'Подобрать оборудование',
+    '/standart': 'Типовые проекты умного дома ',
+    '/items': 'Оборудование Hite-Pro!',
+    '/help': 'Помощь'
+}
