8e84819c761a8eaaccb254ac861fdae7fbeb9009
Vuacheslav <soloomonslava1991@gmail.com>
2024-07-12 16:09:45 +0400

    add set_main_menu def

---
+++ keyboards/main_menu_keyboard.py
@@ -0,0 +1,14 @@
+from lexicon.lexicon_commands import LEXICON_COMMANDS
+from aiogram import Bot
+from aiogram.types import BotCommand
+
+'''Главное меню бота'''
+async def set_main_menu(bot: Bot):
+    main_menu_commands = [
+        BotCommand(
+            command=command,
+            description=description
+        ) for command, description in LEXICON_COMMANDS.items()
+    ]
+
+    await bot.set_my_commands(main_menu_commands)
